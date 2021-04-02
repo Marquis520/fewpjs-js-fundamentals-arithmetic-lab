fd0f821866fed8b647458396897871931d6f861a
Marquis520 <Marquis520@icloud.com>
2021-04-01 22:54:31 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 2
+const num2 = 31
+const num3 = 8
+const num4 = 2
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const mod = 8 / 2
+
+const max = Math.max(20)
